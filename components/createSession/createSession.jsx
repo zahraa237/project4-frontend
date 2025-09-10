9b207d81027b6718e8499296defe8d61ca349128
zahraa <fzahraa237@gmail.com>
2025-09-10 14:53:55 +0300

    added trees pics

--- components/createSession/createSession.jsx
+++ components/createSession/createSession.jsx
@@ -3,21 +3,27 @@ import axios from "axios";
 import "./createSession.css";
 
 function CreateSession() {
-  //   const [user, setUser] = useState(null);
-  //   const [sessions, setSessions] = useState([]);
-  // const [showForm, setShowForm] = useState(false);
   const [title, setTitle] = useState("");
   const [time, setTime] = useState("");
-  const [treeType, setTreeType] = useState("");
+  const [treeType, setTreeType] = useState("/trees/1.jpg");
+  const [showTrees, setShowTrees] = useState(false);
 
   const token = localStorage.getItem("token");
 
-  const createSession = async (e) => {
-    e.preventDefault();
+  const allTrees = [
+    "/trees/1.jpg",
+    "/trees/2.jpg",
+    "/trees/3.jpg",
+    "/trees/4.jpg",
+    "/trees/5.jpg",
+  ];
+
+  // const selectedTree = allTrees.find((tree) => tree === treeType);
+
+  const createSession = async (event) => {
+    event.preventDefault();
 
     try {
-      console.log("Creating session:", { title, time, treeType });
-
       await axios.post(
         `${import.meta.env.VITE_BACKEND_URL}/session/new`,
         { title, time, treeType },
@@ -25,16 +31,13 @@ function CreateSession() {
           headers: { Authorization: `Bearer ${token}` },
         }
       );
-
-      // setShowForm(false);
       setTitle("");
       setTime("");
-      setTreeType("");
-      // fetchData();
     } catch (error) {
       console.error("Failed to create new session :(", error);
     }
   };
+
   return (
     <>
       <div className="modal">
@@ -47,8 +50,8 @@ function CreateSession() {
               type="text"
               placeholder="Title here"
               value={title}
-              onChange={(e) => setTitle(e.target.value)}
-              //required
+              onChange={(event) => setTitle(event.target.value)}
+              required
             />
 
             {/* TIME */}
@@ -56,20 +59,49 @@ function CreateSession() {
               type="number"
               placeholder="Time(minutes)"
               value={time}
-              onChange={(e) => setTime(e.target.value)}
-              //required
+              onChange={(event) => setTime(event.target.value)}
+              required
             />
 
             {/* TREE TYPE */}
-            <input
+            <div className="tree-picker">
+              <img
+                src={treeType}
+                alt="selected tree"
+                className="selected-tree"
+                onClick={() => setShowTrees(true)}
+              />
+            </div>
+
+            {/* <input
               type="text"
               placeholder="Tree Type"
               value={treeType}
-              onChange={(e) => setTreeType(e.target.value)}
-            />
-            <div>
-              <button type="submit">Save</button>
-            </div>
+              onChange={(event) => setTreeType(event.target.value)}
+            /> */}
+
+            {/* choosing a tress */}
+            {showTrees && (
+              <div className="all-trees">
+                <h3>Choose a tree</h3>
+
+                <div className="tree-options">
+                  {allTrees.map((img) => (
+                    <div
+                      key={img}
+                      className="tree-option"
+                      onClick={() => {
+                        setTreeType(img);
+                        setShowTrees(false);
+                      }}
+                    >
+                      <img src={img} alt="Tree" />
+                    </div>
+                  ))}
+                </div>
+              </div>
+            )}
+            <button type="submit">Save</button>
           </form>
         </div>
       </div>
